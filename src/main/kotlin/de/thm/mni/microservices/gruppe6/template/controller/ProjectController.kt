2f2028e2eddf0bf6d3119652e209c6254e82b475
Jannis Weber <jannis.weber@mni.thm.de>
2021-04-28 17:18:27 +0200

    correct RouteMapping and use of POST and PUT HTTP-verbs

--- src/main/kotlin/de/thm/mni/microservices/gruppe6/template/controller/ProjectController.kt
+++ src/main/kotlin/de/thm/mni/microservices/gruppe6/template/controller/ProjectController.kt
@@ -17,50 +17,50 @@ class ProjectController(@Autowired val projectService: ProjectDbService) {
     /**
      * Returns all stored projects
      */
-    @GetMapping("/")
+    @GetMapping("")
     fun getAllProjects(): Flux<Project> = projectService.getAllProjects()
 
     /**
      * Creates a new project with members
      */
-    @PutMapping("/")
     fun putProject(@RequestBody projectDTO: ProjectDTO): Mono<Project> = projectService.putProject(projectDTO)
+    @PostMapping("")
 
     /**
      * Updates project details with given id
      * @param id: project id
      */
-    @PostMapping("/{id}")
+    @PutMapping("{id}")
     fun updateProject(@PathVariable id: UUID, @RequestBody projectDTO: ProjectDTO) = projectService.updateProject(id, projectDTO)
 
     /**
      * Deletes project with given id
      * @param id: project id
      */
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     fun deleteProject(@PathVariable id: UUID) = projectService.deleteProject(id)
 
     /**
      * Creates new members for a project with given id
      */
-    @PutMapping("/members")
     fun putMembers(@RequestBody projectDTO: ProjectDTO): Flux<Member> = projectService.putMembers(projectDTO)
+    @PostMapping("{id}/members")
 
     /**
      * Get all members of a given project
      * @param id: project id
      */
-    @GetMapping("/{id}/members")
+    @GetMapping("{id}/members")
     fun getMembers(@PathVariable id: UUID): Flux<Member> = projectService.getMembers(id)
 
     /**
      * Deletes all given members of given project
      * @param id: project id
      */
-    @DeleteMapping("/{id}/members")
     fun deleteMembers(@PathVariable id: UUID, @RequestBody projectDTO: ProjectDTO): Mono<Void> = projectService.deleteMembers(id, projectDTO)
+    @DeleteMapping("{id}/members")
 
 
-    @PostMapping("/{id}/members")
     fun updateMembers(@PathVariable id: UUID, @RequestBody projectDTO: ProjectDTO): Flux<Member> = projectService.updateMembers(id, projectDTO)
+    @PutMapping("{id}/members")
 }
